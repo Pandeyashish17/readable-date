0e2a00e0f3f3e5ed5744ef5c2deb8b8580cef7d8
Pandeyashish17 <asespandey2@gmail.com>
2023-01-30 21:04:37 +0545

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+function convertDate(date) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const month = months[monthIndex];
+  return `${day} ${month}`;
+}
+module.exports = convertDate;
